4b18d9fc5fcf3ff7e0cb2ee3d67b926280641abe
float <gxhurry@outlook.com>
2022-03-06 15:40:04 +0800

    Temeplete

---
+++ templete.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+using std::cin;
+using std::cout;
+int max(int a, int b)
+{
+    return a < b ? b : a;
+}
+int min(int a, int b)
+{
+    return a < b ? a : b;
+}
