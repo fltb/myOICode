c393de7a7d47d3cb3207c5a833d4ca6e18203522
float <gxhurry@outlook.com>
2022-03-23 22:06:40 +0800

    tem

--- templete.cpp
+++ templete.cpp
@@ -2,11 +2,41 @@
 #include <algorithm>
 using std::cin;
 using std::cout;
-int max(int a, int b)
+template<typename T>
+T max(T a, T b)
 {
     return a < b ? b : a;
 }
-int min(int a, int b)
+template<typename T>
+T min(T a, T b)
 {
     return a < b ? a : b;
 }
+template<typename T, int N>
+class Stack
+{
+    T arr[N];
+    int i;
+public:
+    Stack():i(0) {}
+    void push(const T & item)
+    {
+        arr[i++] = item;
+    }
+    const T & top()
+    {
+        return arr[i-1];
+    }
+    void pop()
+    {
+        i--;
+    }
+    int size()
+    {
+        return i;
+    }
+    bool empty()
+    {
+        return i <= 0;
+    }
+};
