4dca9c97850b242909158d82622deb736abe2155
float <gxhurry@outlook.com>
2022-10-08 21:41:12 +0800

    modify

--- templete.cpp
+++ templete.cpp
@@ -1,56 +1,28 @@
+#include <algorithm>
+#include <cstdio>
 #include <ios>
 #include <iostream>
-#include <cstdio>
-#include <algorithm>
 #define cosnt const;
 using std::cin;
 using std::cout;
-template <typename T> inline void read(T& t) {
-    int f = 0, c = std::getchar(); t = 0;
+template <typename T>
+inline void read(T& t) {
+    int f = 0, c = std::getchar();
+    t = 0;
     while (!std::isdigit(c)) f |= c == '-', c = std::getchar();
     while (std::isdigit(c)) t = t * 10 + c - 48, c = std::getchar();
-    if (f) t = -t;
+    if (f)
+        t = -t;
 }
-template<typename T> inline
-T max(T a, T b)
-{
+template <typename T>
+inline T max(T a, T b) {
     return a < b ? b : a;
 }
-template<typename T> inline
-T min(T a, T b)
-{
+template <typename T>
+inline T min(T a, T b) {
     return a < b ? a : b;
 }
-template<typename T, int N>
-class Stack
-{
-    T arr[N];
-    int i;
-public:
-    Stack():i(0) {}
-    void push(const T & item)
-    {
-        arr[i++] = item;
-    }
-    const T & top()
-    {
-        return arr[i-1];
-    }
-    void pop()
-    {
-        i--;
-    }
-    int size()
-    {
-        return i;
-    }
-    bool empty()
-    {
-        return i <= 0;
-    }
-};
-int main()
-{
+int main() {
     std::ios::sync_with_stdio(false);
 
     return 0;
