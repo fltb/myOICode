e49d568d62ce242fa14e2a70c4d1ad1eb23fd955
float <gxhurry@outlook.com>
2022-10-26 21:38:37 +0800

    AC

--- UVA/11795/main.cpp
+++ UVA/11795/main.cpp
@@ -25,31 +25,41 @@ inline T min(T a, T b) {
 const int MAXN = 16;
 int n;
 int sts[MAXN + 5];
-int dp[1 << MAXN];
-int dfs(int S) {
-    if (sts[0] == S) {
+long long dp[1 << MAXN];
+long long dfs(int S) {  // 被干掉的机器人的集合
+    if (S == ((1 << n) - 1)) {
         return 1;  // 边界——可以消灭
     }
     if (dp[S] != -1) {
         return dp[S];
     }
-    auto res = 0;
-    for (int i = 1; i <= n; i++) {  // 枚举子集~
-        if ((S | sts[i]) == S) {
-            auto preS = S ^ sts[i];
-            res += dfs(preS);
+    auto res = 0ll;
+    auto killable = sts[0];
+    for (auto weapon = 1; weapon <= n; weapon++) {
+        if ((1 << (weapon - 1)) & S) {
+            killable |= sts[weapon];
+        }
+    }
+    for (auto robot = 0; robot < n; robot++) {
+        if (((1 << robot) & (~S)) && ((1 << robot) & killable)) {
+            res += dfs(S | (1 << robot));
         }
     }
     return dp[S] = res;
 }
 int main() {
     std::ios::sync_with_stdio(false);
+    #ifdef FLOATINGBLOCKS
+    freopen("11795.in", "r", stdin);
+    freopen("11795.out", "w", stdout);
+    #endif
     int T;
     cin >> T;
     for (int chesse = 1; chesse <= T; chesse++) {  // 我从 0 开始编号机器壬
         cin >> n;
         cin.get();
         for (int i = 0; i <= n; i++) {
+            sts[i] = 0;
             for (int j = 0; j < n; j++) {
                 char ch;
                 cin >> ch;
@@ -60,11 +70,29 @@ int main() {
         for (auto i = 0; i < (1 << n); i++) {
             dp[i] = -1;
         }
-        auto x = 0;
-        for (auto i = 0; i < n; i++) {
-            x |= (1 << i);
-        }
-        cout << dfs(x) << '\n';
+        cout << "Case " << chesse << ": " << dfs(0) << '\n';
     }
     return 0;
 }
+/*
+1
+16
+1111111111111111
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+
+*/
